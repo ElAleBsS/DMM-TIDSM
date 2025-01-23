b0f34bef012d0551a818654c88d2c46d8b0c0d0b
ElAleBsS <20203i101079@utcv.edu.mx>
2025-01-23 08:32:54 -0600

    Enlazamiento de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+
+  email:string = '';
+  password:string = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(){
+    if(this.email === 'admin' && this.password=== 'admin'){
+      console.log('Login correcto');
+    }else{
+      console.log('Login incorrecto');
+    }
   }
 
 }
